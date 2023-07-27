2bbf7c168b46eea697b5f006933336d7c21dc05d
AKclown <13413629530@139.com>
2023-07-27 17:54:39 +0800

    feat: sentry demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useLocation, useNavigationType, createRoutesFromChildren, matchRoutes } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c6a5ae69a8604c75a084d12d06903efd@o4504015835168768.ingest.sentry.io/4505593191333888",
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      ),
+    }),
+    new Sentry.Replay()
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  release: "AKclown-demo@1.0.0",
+
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+function FallbackComponent() {
+  return <div>An error has occurred</div>;
+}
+
+const myFallback = <FallbackComponent />;
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={({ error, componentStack, resetError }) => {
+      console.log('error, componentStack, resetError: ', error, componentStack, resetError);
+      return myFallback
+    }}>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
 
